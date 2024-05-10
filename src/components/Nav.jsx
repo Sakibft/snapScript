42ff95c084c538fe0d13d7d82cef53c91931f6d2
Sakib <ssar96540@gmail.com>
2024-05-10 13:09:18 +0600

    Added navbar profile pictur || logout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,31 @@
+import { BiLogIn } from "react-icons/bi";
 import { Link, NavLink } from "react-router-dom";
+import UseAuth from "../hooks/UseAuth";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Nav = () => {
+  const [success,setSuccess]=useState();
+  const [erro,setError]=useState();
+  if (success) {
+    return <Link to="/"></Link>;
+  }
+  if(erro){
+    toast.error(erro)
+  }
+  const {user,logOut} = UseAuth()
+  const handleLogOut= () => {
+    logOut()
+    .then((result) => {
+    <Link to='/'></Link>
+      setSuccess(result);
+      console.log(result);
+    })
+    .catch((error) => {
+      setError(error.message);
+    });
+    console.log('ahah');
+  }
    const links = (
     <div className="space-x-8">
     <NavLink
@@ -56,7 +81,7 @@ const Nav = () => {
   </div>
    )
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -107,12 +132,57 @@ const Nav = () => {
           </ul>
         </div>
         <div className="navbar-end space-x-3">
-       <Link to='/register'>
+            {/* dainami login logout */}
+
+        {user ? (
+          <div className="  flex justify-center items-center gap-2">
+              <div className="pointer group relative mx-auto  flex   w-max justify-center">
+              <div className="w-10 border hover:border-blue-500 rounded-full">
+                  <img
+                    className="rounded-full w-full"
+                    src={user?.photoURL}
+                    alt="nai"
+                  />
+                </div>
+            {/* Hover Text */}
+            <div className="absolute -bottom-12 cursor-pointer whitespace-nowrap opacity-0 duration-500 hover:hidden group-hover:-bottom-16 group-hover:opacity-100 z-10 ">
+            <p className=" rounded-md bg-[#8EA7FF] text-white p-2" >
+                    
+                    {user?.displayName} 
+                     </p>
+                <span className="absolute -top-2 left-[50%] h-0 w-0 -translate-x-1/2 -rotate-[45deg] border-b-[20px] border-r-[20px] border-b-transparent border-r-[#8EA7FF]  shadow-[0px_0px_10px_0px_#8EA7FF]"></span>
+            </div>
+            {/* Hover button */}
+        </div>
+           
+           
+            <button
+              onClick={handleLogOut}
+              className="btn text-lg   bg-[#8EA7FF] text-white w-24 "
+            >
+               
+              Logout 
+              
+            </button>
+          </div>
+        ) : (
+         <Link to='/login'>
+          <div className="dropdown dropdown-hover">
+            <button className="btn w-24 bg-[#8EA7FF] text-white hover:bg-[#8EA7FF]">
+              <BiLogIn className="text-lg" /> Login
+            </button>
+          </div>
+         </Link>
+        )}
+
+
+
+       {/* <Link to='/register'>
        <button className="btn">Register</button>
        </Link>
        <Link to='/login'>
        <button className="btn">Login</button>
-       </Link>
+       </Link> */}
         </div>
       </div>
     </div>
